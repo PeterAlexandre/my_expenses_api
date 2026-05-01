1cbe9b868562def5ca418830b966ab4e2fcd22bf
Peter alexandre <petermiguel99@gmail.com>
2026-05-01 18:28:26 -0300

    fix: export all models from __init__ to fix SQLAlchemy mapper resolution

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.transaction import Transaction
+from app.models.transaction_category import TransactionCategory
+from app.models.user import User
+
+__all__ = ["Transaction", "TransactionCategory", "User"]
